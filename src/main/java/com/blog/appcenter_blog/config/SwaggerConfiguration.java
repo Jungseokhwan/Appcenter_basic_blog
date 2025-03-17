a03d1145ba5200e348b958e4386486728647400f
Jungseokhwan <jsh2910@naver.com>
2025-03-17 10:37:13 +0900

    swagger baseurl 설정

--- src/main/java/com/blog/appcenter_blog/config/SwaggerConfiguration.java
+++ src/main/java/com/blog/appcenter_blog/config/SwaggerConfiguration.java
@@ -3,15 +3,24 @@ package com.blog.appcenter_blog.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class SwaggerConfiguration {
 
+    @Value("${swagger.server-url}")
+    private String serverUrl;
+
     @Bean
-    public OpenAPI openAPI(){
+    public OpenAPI openAPI() {
+        Server server = new Server();
+        server.url(serverUrl);
+
         return new OpenAPI()
+                .addServersItem(server)
                 .components(new Components())
                 .info(apiInfo());
     }
@@ -23,3 +32,5 @@ public class SwaggerConfiguration {
                 .version("1.0.0");
     }
 }
+
+
