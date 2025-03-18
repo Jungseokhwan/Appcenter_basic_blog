353792fcc504f7e035da0a76d4295ace0f69ab0f
Jungseokhwan <jsh2910@naver.com>
2025-03-18 23:53:14 +0900

    Login response 값(userId, nickname) 추가

--- src/main/java/com/blog/appcenter_blog/dto/member/LoginResponseDto.java
+++ src/main/java/com/blog/appcenter_blog/dto/member/LoginResponseDto.java
@@ -8,16 +8,25 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class LoginResponseDto {
+
+    private Long userId;
+
     private String loginId;
 
+    private String nickname;
+
     @Builder
-    public LoginResponseDto(String loginId) {
+    public LoginResponseDto(Long userId, String loginId, String nickname) {
+        this.userId = userId;
         this.loginId = loginId;
+        this.nickname = nickname;
     }
 
     public static LoginResponseDto from(MemberEntity member) {
         return LoginResponseDto.builder()
+                .userId(member.getUserId())
                 .loginId(member.getLoginId())
+                .nickname(member.getNickname())
                 .build();
     }
 }
